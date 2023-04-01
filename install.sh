ffdd40c5467f98fbf52025db113f07fe4973d130
Ilán Vivanco <ilanvivanco@gmail.com>
2023-04-01 04:10:23 +0200

    Change the default shell to zsh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt install -y $(cat packages)
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
 
 # install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
